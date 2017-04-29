f09ee7c97e4afe7402425bcbaae36f2472200899
Ike <ikenna.porter@gmail.com>
2017-04-29 16:30:58 +0000

    Boxes with enhancements

--- create_box/main.py
+++ create_box/main.py
@@ -2,6 +2,8 @@
 
 
 def create_box(height, width, character):
+    if height <= 0 or width <= 0:
+        return "Invalid Entry"
     box_string = ""
     for row in range(height):
         for column in range(width):
@@ -9,6 +11,26 @@ def create_box(height, width, character):
         box_string += "\n"
     return box_string
 
+def create_box_empty(height, width, character):
+    if height <= 0 or width <= 0:
+        return "Invalid Entry"
+    box_string = ""
+    for row in range(height):
+        if row == 0 or row == height-1:
+            for column in range(width):
+                box_string += character
+
+        else:
+            for column in range(width):
+                if column == 0 or column == width-1:
+                    box_string += character
+                else:
+                    box_string += " "
+        box_string += "\n"        
+                
+    return box_string
+
 
 if __name__ == '__main__':
     create_box(3, 4, '*')
+    
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,8 @@
 import unittest
 
 from create_box import create_box
+from create_box import create_box_empty
+
 
 first_box_expected = """
 ****
@@ -25,5 +27,43 @@ class TestCreateBox(unittest.TestCase):
 
     def test_small_box(self):
         self.assertEqual(create_box(1, 1, '@'), second_box_expected)
+        
+    def test_small_box_full(self):
+        self.assertEqual(create_box(1, 0, '@'), "Invalid Entry")
 
     # Add your own test using third_box_expected
+    
+    
+
+first_box_expected1 = """
+****
+*  *
+****
+""".lstrip()
+
+second_box_expected1 = """
+@
+""".lstrip()
+
+third_box_expected1 = """
+XXXXXXXXX
+X       X
+X       X
+XXXXXXXXX
+""".lstrip()
+
+
+class TestCreateBox_empty(unittest.TestCase):
+    def test_box(self):
+        self.assertEqual(create_box_empty(3, 4, '*'), first_box_expected1)
+
+    def test_small_box1(self):
+        self.assertEqual(create_box_empty(1, 1, '@'), second_box_expected1)
+        
+    def test_small_box2(self):
+        self.assertEqual(create_box_empty(1, 0, '@'), "Invalid Entry")
+    
+    def test_small_box3(self):
+        self.assertEqual(create_box_empty(4, 9, 'X'), third_box_expected1)
+             
+ 
